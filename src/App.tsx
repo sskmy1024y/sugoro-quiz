1176a1e39dff33fc9b3f8c4e4882ecd42817a203
sho <sskmy1024.y@gmail.com>
2022-11-24 18:21:34 +0900

    ✨ Add firebase environment

--- src/App.tsx
+++ src/App.tsx
@@ -32,6 +32,14 @@ export const App = () => (
             Learn Chakra
           </Link>
         </VStack>
+        <div>{`REACT_APP_APIKEY:${process.env.REACT_APP_APIKEY}`}</div>
+        <div>{`REACT_APP_AUTHDOMAIN:${process.env.REACT_APP_AUTHDOMAIN}`}</div>
+        <div>{`REACT_APP_DATABASEURL:${process.env.REACT_APP_DATABASEURL}`}</div>
+        <div>{`REACT_APP_PROJECT_ID:${process.env.REACT_APP_PROJECT_ID}`}</div>
+        <div>{`REACT_APP_STORAGE_BUCKET:${process.env.REACT_APP_STORAGE_BUCKET}`}</div>
+        <div>{`REACT_APP_MESSAGING_SENDER_ID:${process.env.REACT_APP_MESSAGING_SENDER_ID}`}</div>
+        <div>{`REACT_APP_APP_ID:${process.env.REACT_APP_APP_ID}`}</div>
+        <div>{`REACT_APP_MEASUREMENT_ID:${process.env.REACT_APP_MEASUREMENT_ID}`}</div>
       </Grid>
     </Box>
   </ChakraProvider>
