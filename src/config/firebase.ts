d4d9f54c2a5ba85a6574be401f49000292ac1306
sho <sskmy1024.y@gmail.com>
2022-11-30 11:28:17 +0900

    🐛 fix environment name

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -5,7 +5,7 @@ import { getStorage } from "firebase/storage";
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_APIKEY,
   authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASEURL,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
